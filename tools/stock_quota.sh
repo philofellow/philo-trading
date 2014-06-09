6e55f087c4eab4cdfb989d4f21241face860affd
philofellow <philofellow@gmail.com>
2014-06-09 14:17:38 -0400

    add stock quota pull script

--- tools/stock_quota.sh
+++ tools/stock_quota.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-p=`printf "curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=$1&f=l1'"`
+p=`printf "curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=$1&f=nsl1p2'"`
 price=`eval $p`;
-echo $price
+echo $(date +'%Y/%m/%d %H:%M') $price
