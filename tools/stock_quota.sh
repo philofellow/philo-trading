7adfacc4eb23e0f637b839d6e36bf81ce31a8737
philofellow <philofellow@gmail.com>
2014-06-10 10:13:46 -0400

    update stock quota script

--- tools/stock_quota.sh
+++ tools/stock_quota.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-p=`printf "curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=$1&f=nsl1p2'"`
-price=`eval $p`;
-echo $(date +'%Y/%m/%d %H:%M') $price
+if [ $# == 1 ]
+then
+	p=`printf "curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=$1&f=nsl1p2'"`
+	price=`eval $p`;
+	echo $(date +'%Y/%m/%d %H:%M') $price
+fi
+
+for i in "mcc" "psec" "ta" "alsk" "ntwk" "edmc" "pnnt" 
+do
+	p=`printf "curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=$i&f=nsl1p2'"`
+	price=`eval $p`;
+	echo $(date +'%Y/%m/%d %H:%M') $price
+done
