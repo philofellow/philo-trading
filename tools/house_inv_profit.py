9b2d36b127a3351688f6f8126bf97bee1c615ce3
philofellow <philofellow@gmail.com>
2014-06-11 14:52:34 -0400

    update house value script

--- tools/house_inv_profit.py
+++ tools/house_inv_profit.py
@@ -6,20 +6,20 @@ if len(sys.argv) != 1:
 	print 'USAGE:', sys.argv[0]
 	sys.exit()
 
-rent_m = 1450 # rent per month
-vacancy = 4 # weeks of vacancy per year
-hoa_m = 370 # hoa fee per month
-insurance_y = 400 # all property insurance per year
-house_price = 135000 # property price
+rent_m = 1250.0 # rent per month
+vacancy = 4.0 # weeks of vacancy per year
+hoa_m = 270.0 # hoa fee per month
+insurance_y = 400.0 # all property insurance per year
+house_price = 115000.0 # property price
 downpay = 0.25 # down payment
-upfront_cost = 5000 # closing + refurnish
-int_y = 0.0575 # yearly mortgage interest 
-period_y = 30 # loan period in years
+upfront_cost = 5000.0 # closing + refurnish
+int_y = 0.0475 # yearly mortgage interest 
+period_y = 30.0 # loan period in years
 tax_rate = 0.01 # property tax rate
-inflation = 0.02 # property value increase yearly
+inflation = 0.03 # property value increase yearly
 maintain = 0.1 # maintainess cost in percent of rent
 #agent_fee = rent_m / 12.0 # if use agent to manage
-agent_fee = 0 
+agent_fee = 0.0 
 
 int_m = int_y / 12
 period_m = period_y * 12
@@ -37,26 +37,24 @@ vari_cost_m = rent_m * maintain + rent_m * vacancy / 52.0 + agent_fee
 cost_m = fixed_cost_m + vari_cost_m 
 
 cash_flow = rent_m - cost_m 
+cash_return_y = cash_flow * 12 / init_inv
 principle_pay_m = mortgage_pay_m / 2 # roughly estimate principle payment as a proportion of mortgage payment 
 house_value_inc_m = house_price * inflation / 12
 
 profit_m = cash_flow + principle_pay_m + house_value_inc_m
 return_y = profit_m * 12 / init_inv 
 
-print 'loan payment per month: ' + str(mortgage_pay_m)
-print 'principle payment per month: ' + str(principle_pay_m)
-print 'fixed cost per month (hoa, insurance, mortgage pay, tax): ' + str(fixed_cost_m)
-print 'variable cost per month (maintainess, vacancy, management): ' \
-	+ str(rent_m * maintain) + ' + ' \
-	+ str(rent_m * vacancy / 52.0) + ' + ' \
-	+ str(agent_fee) + ' = ' \
-	+ str(vari_cost_m)
-print 'cost per month: ' + str(cost_m)
-print 'rent per month: ' + str(rent_m)
-print 'cashflow per month: ' + str(cash_flow)
-print 'house value increase per month: ' + str(house_value_inc_m)
-print 'profit per month: ' + str(profit_m)
-print 'initial investment: ' + str(init_inv)
-print 'cash return annually: ' + str(cash_flow * 12 / init_inv)
-print 'total return annually: ' + str(return_y)
+print ('rent per month: %.1f' % rent_m)
+print ('fixed cost per month: %.1f (hoa %.1f, insurance %.1f, mortgage pay %.1f, tax %.1f)' % \
+	(hoa_m, insurance_y / 12, mortgage_pay_m, tax_m, fixed_cost_m))
+print ('variable cost per month: %.1f (maintainess %.1f, vacancy %.1f, management %.1f)' % \
+	(rent_m * maintain, rent_m * vacancy / 52.0, agent_fee, vari_cost_m))
+print ('total cost per month: %.1f' % cost_m)
+print ('cashflow per month: %.1f' % cash_flow)
+print ('principle payment per month: %.1f' % principle_pay_m)
+print ('house value increase per month: %.1f' % house_value_inc_m)
+print ('profit per month: %.1f' % profit_m)
+print ('initial investment: %.1f' % init_inv)
+print ('cash return annually: %.1f%%' % (cash_return_y * 100))
+print ('total return annually: %.1f%%' % (return_y * 100))
 
