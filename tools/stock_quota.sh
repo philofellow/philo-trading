60db484d1e4c495b99281e4f8d5198e65cd67c19
philofellow <philofellow@gmail.com>
2014-11-24 10:07:39 -0500

    update stock quota tool

--- tools/stock_quota.sh
+++ tools/stock_quota.sh
@@ -8,9 +8,27 @@ do
 	echo $(date +'%Y/%m/%d %H:%M') $price
 done;
 
-echo "===== default quotas ====="
+echo "===== indexes ====="
 
-for i in "mcc" "psec" "ta" "alsk" "ntwk" "edmc" "pnnt" "gov"
+for i in "^gspc" "^vix"
+do
+	p=`printf "curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=$i&f=nsl1p2'"`
+	price=`eval $p`;
+	echo $(date +'%Y/%m/%d %H:%M') $price
+done
+
+echo "===== portfolio ====="
+
+for i in "mcc" "gov" "bdcl" "ta" "alsk" "ntwk" "grbk"
+do
+	p=`printf "curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=$i&f=nsl1p2'"`
+	price=`eval $p`;
+	echo $(date +'%Y/%m/%d %H:%M') $price
+done
+
+echo "===== watchlist ====="
+
+for i in "pnnt" "baba" 
 do
 	p=`printf "curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=$i&f=nsl1p2'"`
 	price=`eval $p`;
