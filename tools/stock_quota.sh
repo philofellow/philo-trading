829b736e83a15b6c448d41dc520a4d594a48e062
philofellow <philofellow@gmail.com>
2014-11-24 09:47:56 -0500

    update stock quota tool

--- tools/stock_quota.sh
+++ tools/stock_quota.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ $# == 1 ]
-then
-	p=`printf "curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=$1&f=nsl1p2'"`
+echo "===== queried quotas ====="
+for sym in $@
+do
+	p=`printf "curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=$sym&f=nsl1p2'"`
 	price=`eval $p`;
 	echo $(date +'%Y/%m/%d %H:%M') $price
-fi
+done;
+
+echo "===== default quotas ====="
 
 for i in "mcc" "psec" "ta" "alsk" "ntwk" "edmc" "pnnt" "gov"
 do
